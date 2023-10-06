6f6daf5702a70ac555322d3c60711a76491644b2
Ádám Szrenka <szrenka.adam@gmail.com>
2023-10-06 11:20:34 +0200

    Add project files.

---
+++ Alkalmazottak.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_feladatlap
+{
+    class Alkalmazottak
+    {
+        //name, age, city, department, position, gender, marital status, salary
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string Gender { get; set; }
+        public string Marital { get; set; }
+        public int Salary { get; set; }
+
+        public Alkalmazottak(string sor)
+        {
+            string[] valami = sor.Split(';');
+
+
+            Name = valami[0];
+            Age = Convert.ToInt32(valami[1]);
+            City = valami[2];
+            Department = valami[3];
+            Position = valami[4];
+            Gender = valami[5];
+            Marital = valami[6];
+            Salary = Convert.ToInt32(valami[7]);
+
+        }
+        public void kiir()
+        {
+            Console.WriteLine($"{Name,-8}/ {Age,3}/ {City,10}/ {Department,8}/ {Position,8}/ {Gender,5}/ {Marital,7}/ {Salary, 7} ");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Employee_feladatlap
+{
+    class Program
+    {
+        //8. feladat
+        static double AtlagKor(List<Alkalmazottak> alkalmazott)
+        {
+            double Index = 0;
+            for (int i = 0; i < alkalmazott.Count; i++)
+            {
+                Index += alkalmazott[i].Age;
+            }
+            double eredemeny = Index / alkalmazott.Count;
+            return eredemeny;
+        }
+
+        //9. feladat
+        static int BudapestenElokSzama(List<Alkalmazottak> alkalmazott)
+        {
+            int index = 0;
+            for (int i = 0; i < alkalmazott.Count; i++)
+            {
+                if (alkalmazott[i].City == "Budapest")
+                {
+                    index++;
+                }
+            }
+            return index;
+        }
+
+        //10. feladat
+        static int LegidosebbAlkalmazott(List<Alkalmazottak> alkalmazott)
+        {
+            int legidosebbKora = alkalmazott[0].Age;
+            for (int i = 0; i < alkalmazott.Count; i++)
+            {
+                if (alkalmazott[i].Age > legidosebbKora)
+                {
+
+                }
+            }
+            
+            return legidosebbKora;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Alkalmazottak> alkalmazott = new List<Alkalmazottak>();
+
+
+
+            foreach (var item in File.ReadAllLines(@"../../../adatok.txt"))
+            {
+                alkalmazott.Add(new Alkalmazottak(item));
+            }
+
+            for (int i = 0; i < alkalmazott.Count; i++)
+            {
+                alkalmazott[i].kiir();
+            }
+
+
+            //A következő feladatokat a program osztályban elhelyezett statikus metódusokkal oldd meg. (Aki szeret kísérletezni, teheti ezeket a metódusokat egy újabb osztályba.) Egyes feladatokat meg lehet oldani LINQ-val is, de ha belefér az időbe, kódoljátok le hagyományosan is. Ha van olyan feladat, ami nem egyértelmű, pl. az, hogyan kell kiírni, ott rád van bízva a megoldás.
+
+            //8.     Függvény segítségével írd ki az életkorok átlagát.
+            double atlagKor = AtlagKor(alkalmazott);
+            Console.WriteLine("Az alkalmazottak átlagkora: " + atlagKor);
+
+
+            //9.     Függvény segítségével írd ki azon személyek számát, akiknek a városa 'Budapest'.
+            double OttElok = BudapestenElokSzama(alkalmazott);
+            Console.WriteLine($"A Budapesten élő alkalmazottak száma: {OttElok}");
+
+
+            //10. Függvény segítségével keresd ki, majd a virtuális metódus segítségével írd ki a legidősebb személy adatait.
+            
+
+
+            //11. Függvény segítségével döntsd el, majd a főprogramban írd ki, hogy van-e 30 év fölötti személy, és emellett írd ki a nevét is. (Ez a függvény tehát két értéket kell, hogy generáljon, ezt egyetlen szövegként add vissza a főprogramnak, és a főprogram bontsa szét az adatokat, majd utána írja ki.)
+
+            //12. Függvénnyel válogasd ki azon személyek nevét egy új tömbbe(nem listába), akik 30 évnél fiatalabbak.Ennek a tömbnek a hasznos tartalmát írd ki a főprogramban.
+
+            //13. Egyetlen függvénnyel keresd meg a legfiatalabb és a legidősebb személyt. A függvénynek legyen két olyan paramétere, amiben az eredményt vissza lehet juttatni a főprogramba, és ott ki lehet írni a nevüket és a korukat. A függvény visszatérési értéke pedig képes legyen azt jelezni, hogy van-e több ugyanolyan korú legfiatalabb személy.
+
+            //14. Készíts egy függvényt, ami átszámolja az euróban megadott havi fizetést éves fizetéssé, és az eredményt még váltsd át magyar forintba is.
+
+            //15. Készíts egy függvényt, amelynek visszatérési értéke egy objektumokat tartalmazó lista, amelyben szerepel az 5 millió forint éves fizetés feletti munkavállalók neve és az éves fizetésük forintban. (Az átszámításhoz használd az előző feladat függvényét.)  Az elkészült listát a főprogram írja ki egy új fájlba(a virtuális metódus segítségével).
+
+            //16. Írj egy függvényt, aminek a paramétere az eredeti adatokat tartalmazó listának megfelelő típusú.Ennek segítségével számold ki az összes alkalmazott átlagfizetését.
+
+            //17. Készíts a főprogramban egy olyan listát, amiben csak a developer beosztásúak találhatók, minden tulajdonságukkal. Hívd meg újra a főprogramból az előző függvényt, de most ez az új lista legyen a paramétere.A főprogram írja ki a developerek átlagfizetését.
+
+            //18. Számold ki a férfi és női alkalmazottak átlagfizetését tetszőleges módszerrel.
+        }
+    }
+}
